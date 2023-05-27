380f29f30ec6ee6bae39bd4b87bed5e85b0e0b07
arsitaocti <sitarsita0810@gmail.com>
2023-05-27 08:31:40 -0500

    iyaa ubah

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -1,105 +1,39 @@
-<?php
-
-namespace App\Controllers;
-
-use CodeIgniter\RESTful\ResourceController;
-use App\Models\UserModel; 
-
-class Login extends ResourceController
-{
-    protected $session;
-
-    public function __construct() {
-        $this->userModel = new UserModel();
-        $this->session = \Config\Services::session();
-    }
-
-    /**
-     * Return an array of resource objects, themselves in array format
-     *
-     * @return mixed
-     */
-    public function index()
+public function create()
     {
-        echo view('auth/login');
-    }
+        try {
+            $validate = $this->validate([
+                'email' => 'required',
+                'password' => 'required',
+            ]);
 
-    /**
-     * Return the properties of a resource object
-     *
-     * @return mixed
-     */
-    public function show($id = null)
-    {
-        //
-    }
-
-    /**
-     * Return a new resource object, with default properties
-     *
-     * @return mixed
-     */
-    public function new()
-    {
-        //
-    }
-
-    /**
-     * Create a new resource object, from "posted" parameters
-     *
-     * @return mixed
-     */
-    public function create()
-    {
-        $email = $this->request->getPost('email');
-        $password = $this->request->getPost('password');
-
-        $user = $this->userModel
-                ->where('email', $email)
-                ->first();
-
-        if(!$user) {
-            throw new \Exception("User not found!");
+            if(!$validate) {
+                session()->setFlashData("errors", $this->validator->listErrors());
+                return redirect()->to(previous_url())->withInput();
+            }
+    
+            $email = $this->request->getPost('email');
+            $password = $this->request->getPost('password');
+    
+            $user = $this->userModel
+                    ->where('email', $email)
+                    ->first();
+    
+            if(!$user) {
+                session()->setFlashData("errors", "Email or password is invalid");
+                return redirect()->to(previous_url())->withInput();
+            }
+    
+            if(md5($password) != $user['password']) {
+                session()->setFlashData("errors", "Email or password is invalid");
+                return redirect()->to(previous_url())->withInput();
+            }
+    
+            $this->session->set('id', $user['id']);
+            $this->session->set('name', $user['name']);
+            $this->session->set('loggedIn', true);
+    
+            return redirect()->to('/product');
+        } catch(\Exception $e) {
+            return redirect()->to(previous_url());
         }
-
-        if(md5($password) != $user['password']) {
-            throw new \Exception("Credentials is invalid!");
-        }
-
-        $this->session->set('id', $user['id']);
-        $this->session->set('name', $user['name']);
-        $this->session->set('loggedIn', true);
-
-        return redirect()->to('/product');
     }
-
-    /**
-     * Return the editable properties of a resource object
-     *
-     * @return mixed
-     */
-    public function edit($id = null)
-    {
-        //
-    }
-
-    /**
-     * Add or update a model resource, from "posted" properties
-     *
-     * @return mixed
-     */
-    public function update($id = null)
-    {
-        //
-    }
-
-    /**
-     * Delete the designated resource object from the model
-     *
-     * @return mixed
-     */
-    public function delete($id = null)
-    {
-        //
-    }
-}
