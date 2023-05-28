e704d3b71f70e2a1c2e973785cf8131d28c45ae7
arsitaocti <sitarsita0810@gmail.com>
2023-05-28 02:09:40 -0500

    YUHUU

--- app/Views/layouts/footer.php
+++ app/Views/layouts/footer.php
@@ -1,5 +1,5 @@
 <footer class="footer ">
     <div class="container ">
-        <span class="text-muted ">Place sticky footer content here.</span>
+        <span class="text-muted ">hi HOW ARE YOU BESTIE? YES I KNOW U WANT TO BUY! LETS GOO CHECKIN.</span>
     </div>
 </footer>
--- app/Views/layouts/navbar.php
+++ app/Views/layouts/navbar.php
@@ -1,7 +1,7 @@
 <header>
     <!-- Fixed navbar -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-        <a class="navbar-brand" href="#">Hudya</a>
+        <a class="navbar-brand" href="#">TOK</a>
         <!-- Navbar brand yo -->
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTop" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
